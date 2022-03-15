f402eb92e079de01f5b5a40b8517d4de304fa8ab
Christian Tisby <christiantisby@Christians-MBP.attlocal.net>
2022-03-15 00:29:25 -0400

    adding conditional statements

--- kotlin_standard_library/src/intro.kt
+++ kotlin_standard_library/src/intro.kt
@@ -3,4 +3,35 @@
 fun main() {
     val name = "Bill Clinton"
     var country = "Romania"
+
+    //  Difference between val and var is that variables declared with val cannot be reassigned
+
+    var score: Int //can initialize later
+
+
+    // Null safety
+    //  You can assign null to a variable whenever you want to signal that it doesn't have a value
+//    var car: Car? = null
+//
+//    car = Car("Mercedes Benz")
+//}
+//
+//class Car {
+//
+//}
+
+
+
+    //  Conditional Statements
+    val a = 5
+    val b = 12
+    var max = -1
+
+    if (a > b) {
+    max = a
+    } else {
+        max = b
+    }
+
+    println(max) //prints 12
 }
